40d0f730cfd0bfe00fc87d35f56e37b6bfea60af
mrsrivastavji <srivastav2104shivam@gmail.com>
2026-01-16 09:30:08 +0530

    Array in python

--- array.py
+++ array.py
@@ -1,6 +1,29 @@
 import array
 
-val=array.array('i', [0,1,2,3,4])
+va=array.array('i', [0,1,2,3,4])
 
 for i in range(0,5,1):
-    print(val[i])
+    print(va[i])
+
+val=array.array('i', [0,1,2,3,4,5,6,7,8,9])
+
+val.insert(1,50)
+val.append(100)
+val[5]=25
+
+for x in val:
+    print(x, end=", ")
+
+print()
+
+lav = array.array('i', reversed(val))
+
+for i in range(0, len(lav)):
+    print(lav[i], end=", ")
+
+print()
+
+copylav=array.array(lav.typecode, (x*2 for x in lav))
+
+for i in range(0, len(lav)):
+    print(copylav[i], end=", ")
