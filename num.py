79a26a2ddc114cf1733553c2ea7947822370638f
mrsrivastavji <srivastav2104shivam@gmail.com>
2026-01-22 07:39:30 +0530

    intermediate of array done by help of numpy

---
+++ num.py
@@ -0,0 +1,44 @@
+from numpy import *
+# val=array([1,2,3,4,5])
+
+# for x in val:
+#     print(x, end=" ")
+# print()
+
+# va=linspace(10,20,5)
+
+# for x in va:
+#     print(x, end=" ")
+# print()
+
+# val=arange(10,20,2)
+
+# for x in val:
+#     print(x, end=" ")
+# print()
+
+# val=zeros(10)
+
+# for x in val:
+#     print(x, end=" ")
+# print()
+
+# val=full(10,2)
+
+# for x in val:
+#     print(x, end=" ")
+
+zero=array(10)
+print(zero)
+print()
+
+one=array([1,2])
+print(one)
+print()
+
+two=array([[1,2],[1,2]])
+print(two)
+print()
+
+three=array([[[1,2],[1,2]], [[1,2], [1,2]]])
+print(three)
